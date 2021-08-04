099ea74f96fb52763a0d0224fbc40df73c67a88d
pavanyalala <pavanyalala4508@gmail.com>
2021-08-04 16:10:25 +0530

    [pavan]:Welcome Message Added

---
+++ GenericsToFindMaximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsToFindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to find maximum by using Generics");
+        }
+    }
+}
