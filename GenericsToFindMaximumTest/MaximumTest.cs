478a2fdfcd1f3af14c1ccc96e63f12c3bb458842
pavanyalala <pavanyalala4508@gmail.com>
2021-08-04 17:23:17 +0530

    [pavan]:Integer Maximum Added with test cases

--- GenericsToFindMaximumTest/MaximumTest.cs
+++ GenericsToFindMaximumTest/MaximumTest.cs
@@ -12,13 +12,22 @@ namespace GenericsToFindMaximumTest
         [Test]
         public void GivenMaxNumFirstPosition_whenFindMaxNum_shouldReturnsMaxFirst()
         {
-            int actualMax = FindMaximum.MaxInteger(4508, 1108, 0810);
+            int actualMax = FindMaximum.MaxInteger(4508, 1108, 0811);
+            Assert.AreEqual(4508, actualMax);
+        }
+        /// <summary>
+        /// Test case : 1.2
+        /// </summary>
+        [Test]
+        public void GivenMaxNumSecondPosition_whenFindMaxNum_shouldReturnsMaxSecond()
+        {
+            int actualMax = FindMaximum.MaxInteger(1108, 4508, 0811);
             Assert.AreEqual(4508, actualMax);
         }
         [Test]
-        public void GivenMaxNumSecondPosition_whenFindMaxNum_shouldReturnsMaxSeconf()
+        public void GivenMaxNumThirdPosition_whenFindMaxNum_shouldReturnsMaxThird()
         {
-            int actualMax = FindMaximum.MaxInteger(1108, 4508, 0810);
+            int actualMax = FindMaximum.MaxInteger(0811, 1108, 4508);
             Assert.AreEqual(4508, actualMax);
         }
     }
