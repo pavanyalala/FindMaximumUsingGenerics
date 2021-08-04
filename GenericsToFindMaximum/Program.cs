f8bad5adeaf8c500ad50284d348eca11d6389891
pavanyalala <pavanyalala4508@gmail.com>
2021-08-04 17:59:28 +0530

    [pavan]:String Maximum Test is added

--- GenericsToFindMaximum/Program.cs
+++ GenericsToFindMaximum/Program.cs
@@ -9,6 +9,7 @@ namespace GenericsToFindMaximum
             Console.WriteLine("Welcome to find maximum by using Generics");
             FindMaximum.MaxInteger(1108,4508,0811);
             FindMaximum.MaxFloat(11.08f,45.08f,08.11f);
+            FindMaximum.MaxString("Apple", "Vivo", "Oneplus");
             
         }
     }
